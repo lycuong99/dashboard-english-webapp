7f54ec1fb5b5b4b32564df422c01cbfd220068eb
lycuong99 <52919981+lycuong99@users.noreply.github.com>
2020-10-05 21:48:54 +0700

    Commit V6.0 fix bug lack of token in report page

--- src/main/java/web/app/config/WebConfig.java
+++ src/main/java/web/app/config/WebConfig.java
@@ -11,6 +11,9 @@ public class WebConfig implements WebMvcConfigurer {
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/login").setViewName("login");
         registry.addViewController("/500").setViewName("error/500");
+        registry.addViewController("/401").setViewName("error/401");
+        registry.addViewController("/403").setViewName("error/403");
+        registry.addViewController("/405").setViewName("error/405");
         registry.setOrder(Ordered.HIGHEST_PRECEDENCE);
     }
 }
